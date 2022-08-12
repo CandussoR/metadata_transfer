d6a38e40134a9890b2cd7e7ec32364aa4025728c
Romain Candusso <r.candusso@protonmail.com>
2022-08-12 08:57:15 +0200

    Getting book metadata from the Calibre API.

---
+++ calibre_books.py
@@ -0,0 +1,29 @@
+from calibre.library import db
+import db_config as conf
+import json
+
+calibre_db = db(conf.cal_folder).new_api
+
+book_ids = calibre_db.all_book_ids()
+
+books = []
+
+for book in book_ids:
+    title = calibre_db.field_for("title", book)
+    authors = calibre_db.field_for("authors", book)
+    tags = calibre_db.field_for("tags", book)
+    publishers = calibre_db.field_for("publisher", book)
+    pub_date = calibre_db.field_for("pubdate", book).year
+    books.append( {
+        'title': title,
+        'author': authors,
+        'tags': tags,
+        'publisher': publishers,
+        'pubdate': pub_date
+    })
+
+json_books = json.dumps(books, ensure_ascii=False)
+print(json_books)
+
+with open('json_books.json', 'w') as outfile:
+    json.dump(json_books, outfile, ensure_ascii=False)
